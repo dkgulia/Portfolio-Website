031e2ee3b92f205097b3ad756e4dcf0126623cf8
Dkgulia <dkgulia12@gmail.com>
2024-07-10 18:13:43 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 import 'flowbite';
 import '../css/style.css';
 import './themeToggle';
-import './qualification';
 import './recentWorks';
 import './techSkills';
 import './headerTags';
--- assets/js/techSkills.js
+++ assets/js/techSkills.js
@@ -2,55 +2,47 @@ const techSkills = [
     {
         title: "Languages",
         skills: [
-            { icon: `<i class="fa-brands fa-square-js text-yellow-500"></i>`, name: "JavaScript", label: "Expert" },
-            { icon: `<i class="fa-brands fa-html5 fa-lg text-green-600"></i>`, name: "HTML5", label: "Expert" },
+            { icon: `<i class="fa-brands fa-square-js text-yellow-500"></i>`, name: "JavaScript" },
+            { icon: `<i class="fa-brands fa-html5 fa-lg text-green-600"></i>`, name: "HTML5" },
             { icon: `<i class="fa-brands fa-css3-alt fa-lg text-blue-600"></i>`, name: "CSS3", label: "Expert" },
-            { icon: `<i class="fa-brands fa-php fa-sm text-red-600"></i>`, name: "PHP", label: "Junior" },
-            { icon: `<i class="fa-brands fa-markdown fa-2xs text-slate-600"></i>`, name: "Markdown", label: "Junior" }
+          
         ]
     },
     {
         title: "Front-end",
         skills: [
-            { icon: `<i class="fa-brands fa-react text-yellow-600"></i>`, name: "React", label: "Expert" },
-            { icon: `<i class="fa-solid fa-fire text-red-600"></i>`, name: "Firebase", label: "Expert" },
-            { icon: `<i class="fa-brands fa-bootstrap text-green-600"></i>`, name: "Bootstrap", label: "Expert" },
-            { icon: `<i class="fa-solid fa-code fa-xs text-green-600"></i>`, name: "Tailwind", label: "Expert" },
-            { icon: `<i class="fa-solid fa-code fa-xs text-green-600"></i>`, name: "JQuary", label: "Intermediate" }
+            { icon: `<i class="fa-brands fa-react text-yellow-600"></i>`, name: "React",  },
+            { icon: `<i class="fa-solid fa-fire text-red-600"></i>`, name: "Firebase",  },
+            { icon: `<i class="fa-brands fa-bootstrap text-green-600"></i>`, name: "Bootstrap", },
+            { icon: `<i class="fa-solid fa-code fa-xs text-green-600"></i>`, name: "Tailwind", },
+           
         ]
     },
     {
         title: "Back-end",
         skills: [
-            { icon: `<i class="fa-brands fa-node fa-sm text-green-600"></i>`, name: "Node.js", label: "Expert" },
-            { icon: `<i class="fa-brands fa-node-js text-green-600"></i>`, name: "Express.js", label: "Expert" }
+            { icon: `<i class="fa-brands fa-node fa-sm text-green-600"></i>`, name: "Node.js"},
+            { icon: `<i class="fa-brands fa-node-js text-green-600"></i>`, name: "Express.js" }
         ]
     },
     {
         title: "Database",
         skills: [
-            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "MongoDB", label: "Expert" },
-            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "MySql", label: "Intermediate" },
-            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "Mongoose", label: "Expert" },
+            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "MongoDB" },
+            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "MySql" },
+            { icon: `<i class="fa-solid fa-server fa-xs text-green-700"></i>`, name: "Mongoose" },
         ]
     },
     {
         title: "Dev Tools",
         skills: [
-            { icon: `<i class="fa-brands fa-git text-yellow-500"></i>`, name: "Git", label: "Expert" },
-            { icon: `<i class="fa-brands fa-github"></i>`, name: "GitHub", label: "Expert" },
-            { icon: `<i class="fa-sharp fa-solid fa-laptop-code fa-xs text-blue-500"></i>`, name: "VS", label: "Expert" },
+            { icon: `<i class="fa-brands fa-git text-yellow-500"></i>`, name: "Git" },
+            { icon: `<i class="fa-brands fa-github"></i>`, name: "GitHub" },
+            { icon: `<i class="fa-sharp fa-solid fa-laptop-code fa-xs text-blue-500"></i>`, name: "VS" },
         ]
     },
-    {
-        title: "Design Tools",
-        skills: [
-            { icon: `<i class="fa-solid fa-bezier-curve fa-xs text-yellow-400"></i>`, name: "Adobe Photoshop", label: "Intermediate" },
-            { icon: `<i class="fa-solid fa-bezier-curve fa-xs text-orange-600"></i>`, name: "Adobe Illustrator", label: "Junior" },
-            { icon: `<i class="fa-brands fa-figma  text-orange-400"></i>`, name: "Figma", label: "Junior" },
-            { icon: `<i class="fa-solid fa-file-powerpoint  text-blue-400"></i>`, name: "MS PowerPoint", label: "Expert" },
-        ]
-    },
+   
+
 ];
 const parent = document.getElementById("tech-skills");
 
@@ -59,7 +51,7 @@ const getSkill = (skills) => {
         `<p class="text-md text-gray-700 dark:text-gray-200">
             ${skill.icon}
             ${skill.name}
-            <code class="text-sm text-gray-500 dark:text-gray-300">${skill.label}</code>
+           
         </p>`);
 
     return skillHtml.join(" ");
